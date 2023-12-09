7909ffbf2cfc48e4dadff4c831c2ea84f7cc98d5
Carlos Gómez <carlosgomez2@users.noreply.github.com>
2023-12-09 08:23:41 -0600

    :rotating_light: - lintern fix

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'json'
 require 'typhoeus'
 
+WEBHOOK_URL = 'localhost:3000/webhooks/transactions'.freeze
+
 # Paypool endpoint to process JSON POST request
 post '/paypool' do
   content_type :json
   request.body.rewind
   data = JSON.parse(request.body.read)
   data['status'] = 'paid'
-  "response: #{data.to_json}"
+  request = respond_webhook(data)
+  puts request.body
+  "response_code: #{request.response_code}"
 end
 
 # Strike endpoint to process JSON POST request
@@ -17,5 +21,17 @@ post '/strike' do
   request.body.rewind
   data = JSON.parse(request.body.read)
   data['status'] = 'paid'
-  "response: #{data.to_json}"
+  request = respond_webhook(data)
+  puts request.body
+  "response_code: #{request.response_code}"
+end
+
+def respond_webhook(payload)
+  options = {
+    method: :post,
+    headers: { 'Content-Type' => 'application/json' },
+    body: payload.to_json
+  }
+  request = Typhoeus::Request.new(WEBHOOK_URL, options)
+  request.run
 end
